40666163bafa1a877424be1606aab8e4afb2027b
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-16 20:17:30 +0300

    ex001_1

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+Console.Write("введите число, обозначающее день недели -> ");
+var day = Convert.ToInt32(Console.ReadLine());
+//string[] isWeekend = new string [7];
+string[] isWeekend = { "no", "no", "no", "no", "no", "yes", "yes", };
+if (day > 0 && day < 8)
+{
+    Console.WriteLine(isWeekend[day - 1]);
+}
