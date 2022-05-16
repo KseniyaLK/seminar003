ad79cb42bc7c517ef1be45876079e0a1dd03011c
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-16 21:02:31 +0300

    ex003

---
+++ ex003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер диапазона -> ");
+//int n = int.Parse(Console.ReadLine()); njulf 1.2.3.4 ставим без ковычек
+var n = Console.ReadLine();
+
+if (n == "1")
+{
+    Console.WriteLine("x >0, y >0");
+}
+else if (n == "2")
+{
+    Console.WriteLine("x <0, y >0");
+}
+else if (n == "3")
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (n == "4")
+{
+    Console.WriteLine("x >0, y <0");
+}
